a86c7c29c07a89d5357f9ed40bcd0dd41b2d9e11
Sagar Anand <sagar.anand015@gmail.com>
2018-07-31 12:05:11 +0530

    TODO: fix 404 error

--- src/main/java/com/demo/pushgateway/controllers/ServiceController.java
+++ src/main/java/com/demo/pushgateway/controllers/ServiceController.java
@@ -2,24 +2,21 @@ package com.demo.pushgateway.controllers;
 
 import com.demo.pushgateway.responses.ServiceResponse;
 import io.prometheus.client.Histogram;
-import io.prometheus.client.exporter.PushGateway;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 
 @RestController
 public class ServiceController {
 
-    public static final Histogram requestLatency = Histogram.build().name("requests_latency_seconds")
+    public static  final Histogram requestLatency = Histogram.build().name("requests_latency_seconds")
             .help("Request latency in seconds").register();
 
-    @RequestMapping(value = {"/metrics"})
-    public Response getMetrics(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = {"/hello/world"}, method = RequestMethod.GET)
+    public Response getMetrics() {
         try {
 
 //            PushGateway pg = new PushGateway();
@@ -32,7 +29,8 @@ public class ServiceController {
 
         } catch (Exception e) {
             return Response.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).
-                    entity(new ServiceResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())).
+                    entity(new ServiceResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())).
                     build();
         }
     }
