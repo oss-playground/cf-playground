c0a901fd85b4430414094140aa4b485095c38836
Sagar Anand <sagar.anand@jpmchase.com>
2018-08-31 12:03:40 +0530

    Added remote calls

--- src/main/java/com/playground/demo/springsleuth/controller/DemoController.java
+++ src/main/java/com/playground/demo/springsleuth/controller/DemoController.java
@@ -1,10 +1,14 @@
 package com.playground.demo.springsleuth.controller;
 
+import com.playground.demo.springsleuth.model.ServiceResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,19 +24,35 @@ public class DemoController {
     private RestTemplate restClient;
 
     @RequestMapping(value = "/metrics", method = RequestMethod.GET)
-    public String getMetricsEndpoint(HttpServletRequest request, HttpServletResponse response) {
-        logger.info("in the /metrics API");
-        return "/metrics API response";
+    public ResponseEntity<ServiceResponse> getMetricsEndpoint(HttpServletRequest request, HttpServletResponse response) {
+        logger.info("/metrics API");
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ServiceResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase()));
     }
 
     @RequestMapping(value = "/call", method = RequestMethod.GET)
-    public String callApi(HttpServletRequest request, HttpServletResponse response) {
-        logger.info("in the /call API");
+    public ResponseEntity<ServiceResponse> callApi(HttpServletRequest request, HttpServletResponse response,
+                                                   @RequestParam(value = "local", required = false) boolean isLocal) {
+        logger.info("/call API");
+        String apiResp = "";
+        try {
+            if (!isLocal) {
+                apiResp = restClient.getForObject(System.getenv("DEMO_SERVICE_ONE") + "/metrics", String.class);
+                logger.info("LOCAL API RESPONSE: " + apiResp);
+                return ResponseEntity.status(HttpStatus.OK).body(
+                        new ServiceResponse(HttpStatus.OK.value(), apiResp));
+            }
 
-        String resp = restClient.getForObject("http://localhost:8080/metrics", String.class);
-        logger.info("The response is" + resp);
+            apiResp = restClient.getForObject("http://localhost:8080/metrics", String.class);
+            logger.info("LOCAL API RESPONSE: " + apiResp);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ServiceResponse(HttpStatus.OK.value(), apiResp));
 
-        return "/call API response";
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ServiceResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
+        }
     }
 
 }
---
+++ src/main/java/com/playground/demo/springsleuth/model/ServiceResponse.java
@@ -0,0 +1,33 @@
+package com.playground.demo.springsleuth.model;
+
+public class ServiceResponse {
+
+    private int status;
+
+    private String message;
+
+    public ServiceResponse() {
+        super();
+    }
+
+    public ServiceResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
