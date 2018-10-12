dfe6abafe9659a801e53f21c0e47a557c08d55c5
Sagar  Anand <sagaranand@Sagars-MacBook-Pro-2.local>
2018-10-12 11:43:29 +0530

    Refactored /metrics endpoint code

--- src/main/java/com/demo/pushgateway/controllers/ServiceController.java
+++ src/main/java/com/demo/pushgateway/controllers/ServiceController.java
@@ -3,35 +3,26 @@ package com.demo.pushgateway.controllers;
 import com.demo.pushgateway.responses.ServiceResponse;
 import io.prometheus.client.Histogram;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.core.Response;
-
 @RestController
 public class ServiceController {
 
-    public static  final Histogram requestLatency = Histogram.build().name("requests_latency_seconds")
+    public static final Histogram requestLatency = Histogram.build().name("requests_latency_seconds")
             .help("Request latency in seconds").register();
 
-    @RequestMapping(value = {"metrics"}, method = RequestMethod.GET)
-    public Response getMetrics() {
+    @RequestMapping(value = {"/metrics"}, method = RequestMethod.GET)
+    public ResponseEntity<ServiceResponse> getMetrics() {
         try {
-
-//            PushGateway pg = new PushGateway();
-//            pg.pushAdd();
-
             System.out.println("Hello world!!");
-            return Response.status(HttpStatus.OK.value()).
-                    entity(new ServiceResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase())).
-                    build();
+            return ResponseEntity.ok(new ServiceResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase()));
 
         } catch (Exception e) {
-            return Response.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).
-                    entity(new ServiceResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())).
-                    build();
+            return ResponseEntity.badRequest().body(
+                    new ServiceResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase()));
         }
     }
 
--- src/main/java/com/demo/pushgateway/example/ExampleApplication.java
+++ src/main/java/com/demo/pushgateway/example/ExampleApplication.java
@@ -3,9 +3,10 @@ package com.demo.pushgateway.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.demo.pushgateway.*")
 public class ExampleApplication {
 
     public static void main(String[] args) {
