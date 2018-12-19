006dc42194d6010ebf943d4200a972a90d81700c
Sagar Anand <sagar.anand@jpmchase.com>
2018-12-19 11:58:42 +0530

    Added ss1 code for validating spring sleuth functionality

--- src/main/java/com/demo/pushgateway/example/ExampleApplication.java
+++ src/main/java/com/demo/pushgateway/example/ExampleApplication.java
@@ -3,13 +3,33 @@ package com.demo.pushgateway.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.logging.Logger;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.demo.pushgateway.*")
+// @ComponentScan(basePackages = "com.demo.pushgateway.*")
+@RestController
 public class ExampleApplication {
+
+    private static Logger log = Logger.getLogger(ExampleApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @RequestMapping("/call")
+
+    public String home() {
+        // ServiceInstance instance = lbClient.choose("ss1");
+        RestTemplate rest = new RestTemplate();
+        URI uri = URI.create("http://" + System.getenv("SS1_LINK") + "/metrics");
+        log.info("Calling SS1@" + uri.toString());
+        return rest.getForObject(uri, String.class);
     }
 }
