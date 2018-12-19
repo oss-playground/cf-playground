6e80ecc4d7107d4cde901093c2010cf30175e386
Sagar Anand <sagar.anand@jpmchase.com>
2018-12-19 15:33:51 +0530

    Removed unused deps and other things

--- src/main/java/com/demo/pushgateway/example/ExampleApplication.java
+++ src/main/java/com/demo/pushgateway/example/ExampleApplication.java
@@ -1,11 +1,9 @@
 package com.demo.pushgateway.example;
 
-import org.bouncycastle.cert.ocsp.Req;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
